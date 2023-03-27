c6c67d76b26d0138ebaa3d44b4999bda30f2b21d
gecasado <ger_cp@hotmail.com>
2023-03-27 11:52:39 +0200

    agrego proyecto para crear ramas

---
+++ proyecto1/index.js
@@ -0,0 +1,21 @@
+function suma (op1, op2){
+    let resultado = op1 + op2;
+    console.log(resultado);
+}
+function resta (op1, op2){
+    let resultado = op1 - op2;
+    console.log(resultado);
+}
+function producto (op1, op2){
+    let resultado = op1 * op2;
+    console.log(resultado);
+}
+function division(op1, op2){
+    let resultado = op1 / op2;
+    console.log(resultado);
+}
+function cuadrado(op1) {
+    let resultado = op1 * op1;
+    console.log(resultado);
+}
+module.exports={suma, resta, producto, division,cuadrado}
---
+++ proyecto1/run.js
@@ -0,0 +1,6 @@
+let myLib = require("./index");
+
+myLib.suma(4,2);
+myLib.resta(3,2);
+myLib.producto(5,6);
+myLib.division(9,3);
